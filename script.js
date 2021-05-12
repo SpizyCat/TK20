d94075c064d355af6679e81f6f3059edbe93fc93
SpizyCat <spizy.yt@gmail.com>
2021-05-13 00:19:42 +0200

    Final Project

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-window.onscroll = function() {setSticky();};
-
+var hamburgerButton = document.querySelector("#hamburger-menu-icon");
 var navbar = document.querySelector("#navbar");
-var content = document.querySelector(".content")
-var sticky = navbar.offsetTop;
 
-function setSticky() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-        content.style.paddingTop = "50px";
-
+// Hamburger menu
+function toggleHamburgerMenu() {
+    if (navbar.classList.contains("open")) {
+        navbar.classList.remove("open");
     } else {
-        navbar.classList.remove("sticky");
-        content.style.paddingTop = "0px";
+        navbar.classList.add("open");
     }
-    
 }
+
+hamburgerButton.addEventListener("click", toggleHamburgerMenu);
+
+// Lightbox
 var images = document.querySelectorAll(".image");
-for(var i = 0; i < images.length; i++){
-    images[i].addEventListener("click", e => {
+for (var i = 0; i < images.length; i++) {
+    images[i].querySelector("img").addEventListener("click", (e) => {
         var lightbox = document.querySelector("#img_lightbox");
-        lightbox.style.display = 'grid';
-    
-        lightbox.childNodes[1].src = e.target.currentSrc;
-        console.log(e.childNodes);
-    })
+        lightbox.style.display = "grid";
+
+        var preview_p = document.querySelector("#preview_p");
+        var parent = e.target.parentNode;
+        lightbox.childNodes[1].src = parent.childNodes[1].currentSrc;
+        preview_p.textContent = parent.childNodes[3].textContent;
+    });
 }
 
-
-var preview_p = document.querySelector("#preview_p")  
-
+// Close Lightbox
 var span = document.querySelector(".close");
-span.onclick = function() { 
-    img_lightbox.style.display = "none";
-  }
+if (span) {
+    span.addEventListener("click", () => {
+        img_lightbox.style.display = "none";
+    });
+}
